7f5bdca29f430bea284c800897e6ed4510fbfb04
Gwei Michael <michaelgwei86@gmail.com>
2022-10-26 02:09:20 +0100

    Commiting updated fixed java syntex issues

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,11 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		String name = "Wawa"; 
-		System.out.println (name)
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "wawa";
+        System.out.println(name);
+    }
 }
